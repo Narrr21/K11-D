f28d41e805ae5c1166d853c9a51ff1e42aa33fd8
Nadhif <nadhifalrozin@gmail.com>
2024-05-03 13:18:36 +0700

    feat: penambahan option attack

--- src/battle.py
+++ src/battle.py
@@ -1,24 +1,86 @@
 import os
+import time
 from rng import random
-from share import display,pilihMonster
+from share import pilihMonster, level, get_stats,display
 
-def battle():
+def battle() -> bool:
+    dataBattle = encounter()
+    statAgent:dict = dataBattle[0]
+    statMusuh:dict = dataBattle[1]
+    ronde:int = 0
+    os.system("cls")
+    while True:
+        ronde += 1
+        showStat(statMusuh)
+        showStat(statAgent)
+        turn(ronde, statAgent, statMusuh)
+        showStat(statMusuh)
+        showStat(statAgent)
+        turn(ronde, statMusuh, statAgent, agent=False)
+        check()
+
+def encounter() -> list:
+    statMusuh = musuh()
+    print(f"RAWRR, Monster {statMusuh["Name"]} telah muncul !!!")
+    showStat(statMusuh)
+    userId = 3 #Placeholder
+    namaUser = "x" #placeholder
+    monsterId = pilihMonster(userId, withList=True)
+    levelMonster = level(userId, monsterId)
+    statAgent = get_stats(monsterId, levelMonster)
+    print(f"RAWRR, Agent {namaUser} mengeluarkan monster {statAgent["Name"]} !!!")
+    showStat(statAgent)
+    print("Entering Battle...")
+    time.sleep(2)
+    return [statAgent, statMusuh]
+
+def musuh()-> dict:
     idMusuh = random.generate((1,5))
-    statMusuh = {
-        "Name" : "Zuko", #placeholder
-        "ATK Power" : 20, #placeholder
-        "DEF Power" : 20, #placeholder
-        "HP" : 100, #placeholder
-        "Level" : 1 #placeholder
-    }
-    print("Monster List") #placeholder
-    mosnterId = pilihMonster()
-    statMonster = {
-        "Name" : "Pikachow", #placeholder
-        "ATK Power" : 25, #placeholder
-        "DEF Power" : 5, #placeholder
-        "HP" : 120, #placeholder
-        "Level" : 1 #placeholder
-    }
+    levelMusuh = random.generate((1,5))
+    statMusuh = get_stats(idMusuh, levelMusuh)
+    return statMusuh
+
+def showStat(stat:dict) -> str:
+    display (
+f"""Name      : {stat["Name"]}
+ATK Power : {stat["Atk"]}
+DEF Power : {stat["Def"]}
+HP        : {stat["Hp"]}
+Level      : {stat["Level"]}""")
+
+def turn(number:int, allies:dict, enemies:dict, agent:bool=True):
+    if agent:
+        print(
+f"""
+<============> Turn {number} ({allies["Name"]}) <============>
+1. Attack
+2. Use Potion
+3. Quit
+""")
+        pilihan = int(input("<///> Pilih perintah: "))
+        while True:
+            if pilihan in [1, 2, 3]:
+                if pilihan == 1:
+                    attack(allies["Atk"], enemies["Def"], allies, enemies)
+                    break
+                else:
+                    break
+            else:
+                print("pilihan tidak tersedia!")
+    else:
+        attack(allies["Atk"], enemies["Def"], allies, enemies)
+    os.system("cls")
+
+def attack(Atk:int, Def:int, allies:dict, enemies:dict):
+    lowATK = int(Atk * 7/10)
+    highATK = int(Atk * 13/10)
+    rngATK = random.generate((lowATK, highATK))
+    DEF = rngATK * (Def/100)
+    damage = int(rngATK - DEF)
+    print(f"{allies["Name"]} attack {enemies["Name"]} dealing {damage} damage !!!")
+    time.sleep(4)
+
+def check():
+    ...
 
 battle()
--- src/share.py
+++ src/share.py
@@ -1,15 +1,26 @@
 import os
 
 class YesOrNo:
-    def __init__(self, YN):
+    def __init__(self, masukan:str) -> str:
         while True:
-            if YN != "Y" and YN != "N":
+            if masukan != "Y" and masukan != "N":
                 print("Masukan yang valid hanya Y atau N")
-                YN = input("<///> Y/N: ")
+                masukan = input("<///> Y/N: ")
                 os.system('cls')
             else:
                 break
-        self.YN = YN
+        self.YN = masukan
+
+def get_stats(id:int, level:int) -> dict:
+    stat = {
+        "Name": statMonster(id, 1),
+        "Atk": int(statMonster(id, 2)),
+        "Def": int(statMonster(id, 3)),
+        "Hp": int(statMonster(id, 4)),
+        "Level": level
+    }
+    return stat
+
 
 def display(text:str):
     print(f"""
@@ -17,21 +28,28 @@ def display(text:str):
 {text}
 <==================================================================================>""")
 
-def pilihMonster() -> int:
+def pilihMonster(userId:int, withList:bool=False) -> int:
     # SPESIFIKASI
     # Melakukan loop hingga valid untuk menghasilkan pilihan monster yang ingin diupgrade
     # KAMUS
     # pilihan, level = int
     # ALGORITMA
+    data = monsterInventory()
+    hasil = search(0, str(userId), data)
+    if withList:
+        print("<============> MONSTER LIST <============>")
+        for i in range(len(hasil)):
+            monsterId = int(hasil[i][1])
+            print(f"{i+1}. {statMonster(monsterId, 1)}")
     while True:
         pilihan = int(input("<///> Pilih monster: "))
         os.system('cls')
-        if 0 < pilihan < 5:#5 adalah placeholder untuk panjang list monster
-            return pilihan
+        if 0 < pilihan < len(hasil)+1:
+            return hasil[pilihan-1][1]
         else:
             print("pilihan tidak tersedia!")
 
-def split(baris:str, pemisah:str=None):
+def split(baris:str, pemisah:str=None) -> list:
     if pemisah is None:
         pemisah = " "
     hasil = []
@@ -45,7 +63,7 @@ def split(baris:str, pemisah:str=None):
     hasil.append(temp[:-1])
     return hasil
 
-def splitcsv(file:list):
+def splitcsv(file:list) -> list:
     hasil:list = []
     for line in file:
         row = list(split(line, ";"))
@@ -56,15 +74,32 @@ def monsterInventory() -> list:
     with open(r'data\monster_inventory.csv', 'r') as file:
         return splitcsv(file)
 
+def dataMonster() -> list:
+    with open(r'data\monster.csv', 'r') as file:
+        return splitcsv(file)
+
+def statMonster(monsterId:int, statIndex:int):
+    data = dataMonster()
+    hasil = search(0, str(monsterId), data)
+    return hasil[0][statIndex]
+
 def monsterList(userId:int) -> list:
     data = monsterInventory()
     hasil = search(0, str(userId), data)
-    return hasil
+    print("<============> MONSTER LIST <============>")
+    for i in range(len(hasil)):
+        monsterId = int(hasil[i][1])
+        print(f"{i+1}. {statMonster(monsterId, 1)}")
 
-def search(searchIndex:int, searchInput, file:list) -> list:
+def search(searchIndex:int, searchInput:str, file:list) -> list:
     hasil = []
     for row in file:
         if searchInput == row[searchIndex]:
             hasil.append(row)
     return hasil
+def level(userId:int, monsterId:int):
+    data = monsterInventory()
+    dataUser = search(0, str(userId), data)
+    hasil = search(1, str(monsterId), dataUser)
+    return int(hasil[0][2])
 
